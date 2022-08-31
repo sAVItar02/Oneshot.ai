44e54c8fb7a890249a67cc731841651cce024523
Aviral Singh Chauhan <blr.aviral@gmail.com>
2022-09-01 02:06:57 +0530

    CORS issues #2

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(require("cors"));
+app.use(require("cors")());
 app.use(express.json());
 app.use("/api/v1/colleges", collegeRouter);
 app.use("/api/v1/students", studentRouter);
