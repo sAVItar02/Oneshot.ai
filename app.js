5dd6b53dd11088fde54a9db0ca9d24d38009ecef
Aviral Singh Chauhan <blr.aviral@gmail.com>
2022-09-01 02:04:19 +0530

    CORS fix

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require("express");
+
 const collegeRouter = require("./routes/collegeRouter");
 const studentRouter = require("./routes/studentRouter");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "*");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+app.use(require("cors"));
 app.use(express.json());
 app.use("/api/v1/colleges", collegeRouter);
 app.use("/api/v1/students", studentRouter);
